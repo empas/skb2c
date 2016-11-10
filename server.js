6a3e6dfb3cb1d6d4efbe302406c0d8917fb4bbd0
empas <empas.d@gmail.com>
2016-11-10 17:54:47 +0300

    skb2c

--- server.js
+++ server.js
@@ -5,18 +5,16 @@ const cors    = require('cors');
 const app     = express();
 const port    = 3000;
 
-const re = /\S{1,}/ig;
+// const re = /((https?){0,1}:?(\/\/)?[a-z.]*[a-z]{1,}\.[a-z]{1,}\/)|(\?.*)|@|(\/.*)/ig;
+// const re = /((https?){0,1}:?(\/\/)?[a-z.]*[a-z]{1,}\.[a-z]{1,}\/)|@/i;
+const re = /^((https?)?:?(\/\/)?([\w-.]*)?\/)/i;
 
-let parseName = (lastname, ...rest) => {
+let parseUserName = (url) => {
 
-    let cuttedNames = rest.map((name) => {
-        let initial = name[0].toUpperCase()+'.';
-        return initial;
-    });
-    let tmpLastname = lastname[0].toUpperCase() + lastname.substr(1).toLowerCase();
-    let modName = `${tmpLastname} ${cuttedNames.join(' ')}`;
+    let username = url.replace(re, '');
+    username = username.replace(/(\/.*)|(\?.*)|@/ig, '');
 
-    return modName;
+    return '@'+username;
 
 };
 
@@ -26,22 +24,21 @@ app.get('/',(req, res) => {
     console.log('req.query');
     console.log(req.query);
 
-    // let namesStr = req.query.fullname.replace(/\d/g,'');
-    let namesStr = req.query.fullname;
-    let names = namesStr.match(re);
-    // console.log(names);
+    let url = req.query.username;
 
-    if(!names || names.length > 3 || /[\d_/]/.test(namesStr)){
-        console.log('Invalid fullname');
-        res.send('Invalid fullname');
-    }else{
-        let lastname = names.pop();
+    res.send(parseUserName(url));
 
-        let modifiedName = parseName(lastname, ...names);
-        console.log(modifiedName);
-
-        res.send(modifiedName.trim());
-    }
+    // if(!names || names.length > 3 || /[\d_/]/.test(namesStr)){
+    //     console.log('Invalid fullname');
+    //     res.send('Invalid fullname');
+    // }else{
+    //     let lastname = names.pop();
+    //
+    //     let modifiedName = parseName(lastname, ...names);
+    //     console.log(modifiedName);
+    //
+    //     res.send(modifiedName.trim());
+    // }
 });
 
 const server = app.listen(port);
